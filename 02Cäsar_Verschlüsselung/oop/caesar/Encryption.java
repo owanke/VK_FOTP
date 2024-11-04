af8eb1d323b0f3fbf252cbe35f8f8e6d17dff418
Oliver Wanke <oliver.wanke@acpp.de>
2024-11-04 22:44:32 +0100

    02 Caeser läuft

--- 02Cäsar_Verschlüsselung/oop/caesar/Encryption.java
+++ 02Cäsar_Verschlüsselung/oop/caesar/Encryption.java
@@ -9,14 +9,37 @@ public class Encryption
         
     }
 
-    public String encode(String quelle, int x) {
-        String Ergebnis = quelle + Integer.toString(x);
-        return Ergebnis;
+    public static String encode(String quelle, int x) 
+    {
+        StringBuilder result= new StringBuilder(quelle);
+        // quelle to lowercase
+        quelle = quelle.toLowerCase();
+        
+        // iterate through string
+        for (int i=0; i<quelle.length();i++)
+        {
+            char symbol = quelle.charAt(i);
+            int numberOfSymbol=symbol;
+            int newSymbol = numberOfSymbol+(x%26);
+            if (newSymbol>122)
+            {
+                newSymbol = newSymbol-26;
+            }
+            if (newSymbol<97)
+            {
+                newSymbol = newSymbol+26;
+            }
+           
+            if ((numberOfSymbol<123) && (numberOfSymbol>96))
+            {
+                result.setCharAt(i, (char)(newSymbol));
+            }
+            
+        }
+        System.out.println(result);
+        return result.toString();
     }
     
-    public String lowerCase(String original)
-    {
-        return original.toLowerCase();
-    }
+   
    
 }
--- 02Cäsar_Verschlüsselung/oop/caesar/EncryptionTest.java
+++ 02Cäsar_Verschlüsselung/oop/caesar/EncryptionTest.java
@@ -5,20 +5,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EncryptionTest {
+class EncryptionTest 
+{
 
 
 
     @Test
     void checkEncryption()
     {
-        assertTrue(new Encryption().encode("ein Anfang",2).equals("ein Anfang2"));
+        assertTrue(new Encryption().encode("ab",1).equals("bc"));
+        assertTrue(new Encryption().encode("bc",-1).equals("ab"));
+        assertTrue(new Encryption().encode("b c",-1).equals("a b"));
+        assertTrue(new Encryption().encode("a",-1).equals("z"));
+        assertTrue(new Encryption().encode("X19OsY2gkmQhegGK9H",61).equals("g19xbh2ptvzqnppt9q"));
+        assertTrue(new Encryption().encode("Z7kMwD1;lrRfa 4?YQgUBzYHLTw6.Jmr7 JJ9yYyV?3JRqIMh",-38).equals("n7yakr1;zffto 4?meuipnmvzhk6.xaf7 xx9mmmj?3xfewav"));
+
     }
     
-    @Test
-    void lowerCase()
-    {
-        assertTrue(new Encryption().lowerCase("ABC").equals("abc"));
-    }
+    
+    
+   
                     
 }
